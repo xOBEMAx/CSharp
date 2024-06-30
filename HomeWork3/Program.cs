47b32fc099c99416e113f806f9fa5d5db69022cd
xOBEMAx <dr.martens161@gmail.com>
2024-06-30 12:57:28 +0200

    HomeWork3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double firstValue, secondValue;
+string action;
+
+Console.WriteLine("Введите число 1");
+firstValue = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+secondValue = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+action = Console.ReadLine();
+
+switch (action)
+{
+    case "+":
+        Console.WriteLine(firstValue + secondValue); 
+        break;
+
+    case "-":
+        Console.WriteLine(firstValue - secondValue);
+        break;
+
+    case "*":
+        Console.WriteLine(firstValue * secondValue);
+        break;
+
+    case "/":
+        if (secondValue == 0) ;
+        {
+            Console.WriteLine(0);
+        }
+
+        Console.WriteLine(firstValue / secondValue);
+        break;
+     
+
+
+
+
+    default:
+        Console.WriteLine("Ошибка! Неизвестное действие!");
+        break;
+}
+Console.ReadLine();
+
+  
