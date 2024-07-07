b255fd3e226a298f16a497ba851d8e294b33eda3
xOBEMAx <dr.martens161@gmail.com>
2024-07-07 09:36:16 +0200

    Действия с Матрицей

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,6 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.ComponentModel.Design;
+using System.Data;
+using System.Text;
 
-while (true) ;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = "Оплата по счёту №189 от 10.01.2022 за транспортные услуги в т. ч. НДС 20% 1564,89";
 
-double firstValue, secondValue;
+        var newStr = str.Split(' ');
+        foreach (var item in newStr) 
+        {
+            if (item.StartsWith('№'))
+            {
+                Console.WriteLine(item);
+            }
+            if (DateOnly.TryParse(item, out var date))
+            {
+                Console.WriteLine(date);
+            }
 
+
+
+        }
+
+    }
+}
---
+++ HomeWork4/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Data;
+using System.Numerics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write(" Введите количество строк матрицы: ");
+        var rows = int.Parse(Console.ReadLine());
+        Console.Write(" Введите количество столбцов матрицы: ");
+        var columns = int.Parse(Console.ReadLine());
+        int[,] array = new int[rows, columns];
+
+
+        Console.WriteLine(" Введите элементы в таблицу: ");
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($" Элемент [{i},{j}]: ");
+                array[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.Clear();
+        Console.Write(" Ваш массив: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < columns; j++)
+            {
+
+                Console.Write(" {0} ", array[i, j]);
+            }
+        }
+        Console.WriteLine("\n Выберите действие:" +
+            "\n1 - Найти количество положительных чисел в матрице: " +
+            "\n2 - Найти количество отрицательных чисел в матрице: " +
+            "\n3 - Сортировка элементов матрицы построчно  (в двух направлениях): " +
+            "\n4 - Инверсия элементов матрицы построчно: ");
+
+
+        var operation = int.Parse(Console.ReadLine());
+        switch (operation)
+        {
+            case 1:
+                Console.Write("положительные числа: ");
+                int countPositive = 0;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        if (array[i, j] > 0)
+                        {
+                            Console.Write(" {0} ", array[i, j]);
+                            countPositive++;
+                        }
+                    }
+                }
+
+                Console.WriteLine("\nКоличество положительных чисел: " + countPositive);
+                break;
+
+            case 2:
+                Console.Write("отритцательные числа: ");
+                int countNegative = 0;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        if (array[i, j] < 0)
+                        {
+                            Console.Write(" {0} ", array[i, j]);
+                            countNegative++;
+                        }
+                    }
+                }
+
+                Console.WriteLine("\nКоличество отрицательных чисел: " + countNegative);
+                break;
+
+            case 3:
+                Console.WriteLine("Сортировка элементов матрицы построчно ");
+                
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = i + 1; j < array.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < array.GetLength(1) - j - 1; k++)
+                            if (array[i, k] > array[i, k + 1])
+                            {
+                                int t = array[i, k];
+                                array[i, k] = array[i, k + 1];
+                                array[i, k + 1] = t;
+                            }
+
+                    }
+                }
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
+
+    }
+
+}
+
+
+
+
+
+
+/*
+Console.Write("Введите колличество чисел: ");
+        int n = int.Parse(Console.ReadLine());
+int countNegativ = 0;
+        for (int i = 0; i<n; i++)
+        {
+            Console.Write($"Введите число {i + 1}: ");
+            int x = int.Parse(Console.ReadLine());
+            if (x< 0)
+                countNegativ++;
+        }
+        Console.WriteLine($"Отритцательных чисел: {countNegativ}");
+
+
+Console.Write("Введите колличество чисел: ");
+int t = int.Parse(Console.ReadLine());
+int countPositive = 0;
+for (int i = 0; i > n; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        countPositive++;
+}
+Console.WriteLine($"Положительных чисел: {countPositive}");*/
+
+
+
+
+
+
+
+
+
+
+
