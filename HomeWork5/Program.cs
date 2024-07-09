6f04b49fd456c1bc1fc1247e0f2e744d384d29f9
xOBEMAx <dr.martens161@gmail.com>
2024-07-09 19:37:36 +0200

    Матрица

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int rowNumber, colNumber;
+
+        while (true)
+        {
+            Console.WriteLine("Введите количество строк");
+
+            var userInput = Console.ReadLine();
+            
+            switch (userInput)
+            {
+                case "1":
+                    {
+                        for (int i = 0; i < 10; i++) 
+                    }
+                    break;
+
+               
+            }
+            
+        }
+    }
+}
